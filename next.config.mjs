662932539082e954888f6a45ebdd761ca4557555
rushi <rushikesh@markiversemedia.com>
2024-10-23 15:46:25 +0530

    updated NEXT_PUBLIC_APP_URL to nextconfig file

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_APP_URL: 'https://outstatic-blogs.vercel.app',
+        NEXT_PUBLIC_APP_URL: 'https://outstatic-blogs-rushi.vercel.app/',
     },
 };
 
